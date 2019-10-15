d8e7411eb6e87a47938c60001674a4bde4cb5b76
megafriday <megafriday@yahoo.co.jp>
2019-10-16 01:31:58 +0900

    Initial Commit

---
+++ src/main/java/scraping_java/Main.java
@@ -0,0 +1,12 @@
+package scraping_java;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scraping scraping = new Scraping();
+		scraping.start();
+	}
+
+}
---
+++ src/main/java/scraping_java/Scraping.java
@@ -0,0 +1,48 @@
+package scraping_java;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Scraping {
+
+	final private String selector = "#primary > div > div > div > article > header > h2 > a";
+
+	public void start() throws IOException {
+
+		// 対象ページ
+		ArrayList<String> urlList = new ArrayList<String>();
+		urlList.add("https://www.sejuku.net/blog/category/programing/node-js");
+		urlList.add("https://www.sejuku.net/blog/category/programing/node-js/page/2");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/2");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/3");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/4");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/5");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/6");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/7");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/8");
+		urlList.add("https://www.sejuku.net/blog/category/programing/javascript/page/9");
+
+		for (String url : urlList) {
+			getTitle(url);
+		}
+
+	}
+
+	private void getTitle(String url) throws IOException {
+
+		Document document = Jsoup.connect(url).get();
+		Elements elements = document.select(selector);
+
+		for (Element element : elements) {
+			System.out.println(element.text());
+		}
+
+		System.out.println("------------------------------------------------------");
+	}
+}
